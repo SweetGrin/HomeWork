7514fe7c52418a4e24f5aedac9435412ab6e83ed
Olga <o-2010shevchenko@mailk.ru>
2022-09-02 21:20:50 +0300

    seminar

---
+++ DZ6/Program.cs
@@ -0,0 +1,36 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+int NumberСounter(int[] array)
+{
+ int sum=0;
+ for(int i=0; i<array.Lenght; i++)
+ {
+   if(array[i]>0)
+   sum++;
+ }
+ return sum;
+}
+int[] CreateRandomArray(int size)
+{
+    int[] NewArray=new int[size];  
+
+    for(int i=0; i<size; i++)
+    {
+      
+       Console.Write($"Введите число: ");
+       NewArray[i]=Convert.ToInt32(Console.ReadLine());    
+    }
+    return NewArray;
+}
+void ShowArray(int[] array)
+{
+  for(int i=0; i<array.Length; i++)
+      Console.Write(array[i] +" ");
+  Console.WriteLine();
+}
+
+Console.Write("Введите окличество чисел для проверки: ");
+int a= Convert.ToInt32(Console.ReadLine());
+int[] myArray=CreateRandomArray(a);
+ShowArray(myArray);
+int result=NumberСounter(myArray);
+Console.Write($"Количество чисел больit 0 равно {result}");
--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -39,5 +39,62 @@
 
 
 //Принимает 3 числа. может ли существовать треугольник такой величины.
+// bool CheckTriangle(int x, int y, int z)
+// {
+//   if((x<y+z) && (z<x+y)&&(y<x+z))
+//      return true;
+//   else
+//   {
+//     return false;
+//   }
+// }
+// Console.WriteLine("Input side size A : ");
+// int a= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input side size B: ");
+// int b= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input side size C: ");
+// int c= Convert.ToInt32(Console.ReadLine());
+// bool result=CheckTriangle(a,b,c);
+// Console.WriteLine(result);
 //не используя рекурсию вывести первое N чисел отряда ФИбаначи.
+// int[] Fibonachi(int a, int b,int N)
+// {
+//     int[] array=new int[N];
+//     array[0]=a;
+//     array[1]=b;
+//     for( int i=2; i<N; i++)
+//     {
+//       array[i]=array[i-2]+array[i-1];
+//     }
+//     return array;
+// }
+// void ShowArray(int[] array)
+// {
+//   for(int i=0; i<array.Length; i++)
+//       Console.Write(array[i] +" ");
+//   Console.WriteLine();
+// }
+// Console.WriteLine("Input 1 number: ");
+// int firstnumber= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input 2 number: ");
+// int secondNumber= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input size for array: ");
+// int size= Convert.ToInt32(Console.ReadLine());
+// ShowArray(Fibonachi(firstnumber,secondNumber,size));
+
 //программа которая будет преобразововать 10 системы в 2ричную систему.
+// void Sistem(int number)
+// {
+//    string result=string.Empty;
+//    while(number>0)
+//    {
+//      result=number%2 + result;
+//      number/=2;
+//    }  
+//    Console.WriteLine(result);
+// }
+// Console.WriteLine("Input  number: ");
+// int num= Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Number {num} to binaty is ");
+
+// Sistem(num);
